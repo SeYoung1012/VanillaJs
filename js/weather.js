e6b4491b005844500db6d844454d71a6a56568b3
SeYoung1012 <carvensandro@gmail.com>
2022-08-19 14:40:18 +0900

    바닐라 js 공부 끝

--- js/weather.js
+++ js/weather.js
@@ -1,26 +1,23 @@
 const API_KEY = "4d4bb590a6ceb3d90dbf57c5fe642e97";
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
 
 //success함수는 geolocation position object를 하나만 입력 받음
 
-function onGeoOk(position){
+function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-   
-    fetch(url).then(response => response.json()).then(data => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
 
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-           city.innerText = data.name;
-           weather.innerText = data.weather[0].main;
-        
+       
 
-        });
-
-
-
-   
-}
+      });
+  }
 
 function onGeoError(){
     alert("Can't find you. No weather for you.");
